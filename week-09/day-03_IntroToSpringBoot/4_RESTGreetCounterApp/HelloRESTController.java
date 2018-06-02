358516516de8e8c9a49492344423fb9c5f50f209
AgnesKazai <agnes.kazai0325@gmail.com>
2018-06-02 19:39:39 +0200

    add(Spring boot tasks)

---
+++ week-09/day-03_IntroToSpringBoot/4_RESTGreetCounterApp/HelloRESTController.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloRESTController {
+    AtomicLong atomicLong = new AtomicLong(1);
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam("name") String name) {
+
+        long id = atomicLong.getAndIncrement();
+        return new Greeting(id, "Hello, " + name);
+    }
+}
